a0a6eee1461228de4478ab23c3b3cae23b45d3bf
groveincharge <wdasent@yahoo.com>
2019-09-02 19:16:42 -0400

    update packge.json and other files

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 import ItemModal from './components/ItemModal';
 import {Container} from 'reactstrap';
 import {Provider} from 'react-redux';
 import store from './store';
-
 import './App.css';
 
 function App() {
