052242d25d85a8e0974ca90b8302376202222ef1
groveincharge <wdasent@yahoo.com>
2019-09-02 16:13:26 -0400

    First commit of the heroku deploy.

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ if(process.env.NODE_ENV === 'production') {
              res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
        })      
 };
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 5000;
 app.listen(port, ()=> console.log(`Server started on port ${port}`));
